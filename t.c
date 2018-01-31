314b964367f595b446394c59e29e71bd7701d8de
jaganjaga <35915157+jaganjaga@users.noreply.github.com>
2018-01-31 14:45:56 +0530

    Create t.c

---
+++ t.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("enter a integer:");
+  scanf("%d",number);
+  printf("result:%s",odd even(number));
+  return 0;
+}
+char*odd even(int number)
+{
+if(number%2==0);
+printf("your number is even number");
+else
+printf("your numbrt is odd");
+}
+  
